58b6686068089add66b446f11961b6b8208f14b3
Hawer12340 <158380160+Hawer12340@users.noreply.github.com>
2025-11-04 20:26:06 -0300

    Add files via upload

---
+++ barbearia/src/php/cadastro.php
@@ -0,0 +1,46 @@
+ <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Um site Sobre Barbearia Corte de Cabelo, Sombrancelha, Barba">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href='https://cdn.boxicons.com/3.0.3/fonts/basic/boxicons.min.css' rel='stylesheet'>
+    <title>Tela de Cadastro</title>
+</head>
+<body>
+
+<div>
+    <h1>Cadastre-se Já!</h1>
+<div>
+
+    <form action="" method="post">
+
+<div>
+    <p>Usuário</p>
+    <input type="text" name="Nome" id="Nome" placeholder="Nome">
+    <i class='bxr  bx-user'    ></i> 
+    <div>
+
+<p>Email</p>
+    <input type="text" name="Email" id="Email" placeholder="Email">
+    <i class='bxr  bx-mail-open'    ></i>  
+    <div>
+
+    <div>
+    <p>Senha</p>
+    <input type="password" name="Senha" id="Senha" placeholder="Senha">
+    <i class='bxr  bx-lock'    ></i> 
+    <div>
+
+    <div>
+    <p>Lembrar Senha<p>
+    <input type="checkbox" name="lembrarSenha" id="lembrarSenha">
+    <input type="submit" value="">
+    <div>
+
+    </form>
+
+
+</body>
+</html>
---
+++ barbearia/src/php/index.php
@@ -0,0 +1,44 @@
+ <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <meta name="description" content="Um site Sobre Barbearia Corte de Cabelo, Sombrancelha, Barba">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/src/css/index.css">
+     <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+
+    <title>Tela de Login</title>
+</head>
+<body>
+    
+
+    <form action="" method="post">
+<h1 class="login">LOGIN</h1>
+    <p>Email</p>
+    <input type="email" name="email" id="email" placeholder="Email">
+    <img id="imgEmail" src="/src/img/HyperX-Icon-Logo.png" alt="logo do email">
+    <p>Senha</p>
+   <input type="password" name="Senha" id="Senha" placeholder="Senha">
+   <img id="imgSenha" src="/src/img/HyperX-Icon-Logo.png" alt="logo da senha">
+    <span class="lnr lnr-eye"></span>
+    <br>
+    <input type="checkbox" name="lembrarSenha" id="lembrarSenha">
+  
+    <label for="lembrarSenha">Lembrar Senha</label>
+   
+    <a href="">Esqueceu a Senha?</a>
+    <br>
+    <input type="submit" value="Login">
+    <p>Não tem uma conta?<a href="cadastro.php">Cadastrar-se</a></p>
+    </form>
+
+<footer>
+<h2>Nome da empresa</h2>
+<img id="logoImg" src="/src/img/HyperX-Icon-Logo.png" alt="logo">
+</footer>
+</body>
+</html>
