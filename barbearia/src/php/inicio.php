ce95925b586bc5a2763a5ba3436363a083697c22
Hawer Ferreira <ferreirahawer20@gmail.com>
2025-11-13 02:11:33 -0300

    novas paginas

--- barbearia/src/php/inicio.php
+++ barbearia/src/php/inicio.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>inicio</title>
 </head>
 <body>
-    
+    <div>
+    <img id="menuHamburguer" alt="menu Hamburguer" srcset="/barbearia/src/img/menu.png">        
+</div>
 
 
 
