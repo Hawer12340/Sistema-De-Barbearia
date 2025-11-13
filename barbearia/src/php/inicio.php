ceb1e19298a94e3b9be9802b09bf052001918f94
Hawer Ferreira <ferreirahawer20@gmail.com>
2025-11-13 02:29:23 -0300

    teste

--- barbearia/src/php/inicio.php
+++ barbearia/src/php/inicio.php
@@ -8,18 +8,85 @@ $nome = "Hawer";
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-
+ <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Jura:wght@300..700&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/barbearia/src/css/inicio.css">
     <title>inicio</title>
 </head>
 <body>
     <header>
     <img id="menuHamburguer" alt="menu Hamburguer" srcset="/barbearia/src/img/menu.png"> 
     <img id="carrinho" alt="carrinho de compras" srcset="/barbearia/src/img/carrinho.png">  
-    <p>Seja Bem Vindo, <?php echo $nome;?></p>     
+    <p id="bemVindo">Seja Bem Vindo, <?php echo $nome;?></p>     
 </header>
+<main>
+<h1>Ofertas</h1>
 
+<div>
+    <!-- Oferta 1 -->
+    <p>Oferta 1</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+   <!-- Oferta 2 -->
+    <p>Oferta 2</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+       <!-- Oferta 3 -->
+    <p>Oferta 3</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+       <!-- Oferta 4 -->
+    <p>Oferta 4</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+</div>
+
+ <!-- Div 2 -->
+<div>
+    <!-- Oferta 1 -->
+    <p>Combos 1</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+   <!-- Oferta 2 -->
+    <p>Combos 2</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+       <!-- Oferta 3 -->
+    <p>Combos 3</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+       <!-- Oferta 4 -->
+    <p>Combos 4</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+</div>
+
+ <!-- Div 3 -->
+<div>
+    <!-- Oferta 1 -->
+    <p>Plano 1</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+   <!-- Oferta 2 -->
+    <p>Plano 2</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+       <!-- Oferta 3 -->
+    <p>Plano 3</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+       <!-- Oferta 4 -->
+    <p>Plano 4</p>
+    <img alt="" srcset="">
+    <button type="button"></button>
+</div>
+
+
+
+
+
+</main>
 
 
 
